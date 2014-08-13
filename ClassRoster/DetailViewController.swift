7029f5fa9836240e5bba06f4166537837c439ca6
Adrian Gherle <adriangherle@Adrians-MacBook-Pro.local>
2014-08-13 13:57:17 -0700

    Added DetailViewController class, when a cell is selected a new segue opens as DetailViewController

---
+++ ClassRoster/DetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  ClassRoster
+//
+//  Created by Adrian Gherle on 8/13/14.
+//  Copyright (c) 2014 Adrian Gherle. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
